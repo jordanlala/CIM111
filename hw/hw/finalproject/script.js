45dc5f3e7b48c44a0af5afae3622b54107aa1626
jordan lala <jxl1945@miami.edu>
2019-12-09 18:26:13 -0500

    final updates

---
+++ hw/hw/finalproject/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  $("button").click(function() {
+    $("p").hide();
+  });
+
+  // $("h2").mouseover(function(){
+  //   $("h2").css("background-color", "blue");
+  // });
+
+  $("h1").mouseout(function(){
+    $("h1").css("background-color", "black");
+  });
+
+  $("button").click(function(){
+    $("h2").hide();
+  });
+
+$("#brews").mouseover(function() {
+  $("#brews").toggle(1000);
+  console.log("")
+});
+});
